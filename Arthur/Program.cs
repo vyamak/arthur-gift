6cacf6ccf52701adff407a7df5edb23f29b7eba9
Vincent YAMAK <vyamak@avanteam.fr>
2023-12-15 15:07:07 +0100

    Update Program.cs

--- Arthur/Program.cs
+++ Arthur/Program.cs
@@ -5,7 +5,7 @@ titi.Database.CreateSchema();
 titi.Children.Add(new Child
 {
     Name = "Arthur",
-    BornAt = new DateTime(2023, 12, 14),
+    BornAt = new DateTime(2023, 12, 131),
     WeightInKg = 2.3,
     HeightInCm = 47,
 });
